6ce6f1f957f3eebe6e2d529b29f57e669883b7c3
xxcyou3 <980334400@qq.com>
2024-09-17 04:05:27 +0800

    you

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+
+const app = express();
+
+//添加 CORS 中间件
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+app.get("/api",(req,res)=>{
+  res.end("hello");
+})
+
+
+export default app;
